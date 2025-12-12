5334f147e488b6a5037090dcbcd90bb237240f95
Psikuvit <thegengestar@gmail.com>
2025-12-12 23:37:04 +0100

    Update: Added a key for supply drops rewards

--- src/main/java/me/psikuvit/cashClash/util/Keys.java
+++ src/main/java/me/psikuvit/cashClash/util/Keys.java
@@ -8,6 +8,7 @@ public class Keys {
     public static final NamespacedKey SHOP_ITEM_KEY = new NamespacedKey(CashClashPlugin.getInstance(), "shop_item");
     public static final NamespacedKey SHOP_BOUGHT_KEY = new NamespacedKey(CashClashPlugin.getInstance(), "shop_bought");
     public static final NamespacedKey SHOP_NPC_KEY = new NamespacedKey(CashClashPlugin.getInstance(), "shop_npc");
+    public static final NamespacedKey SUPPLY_DROP_AMOUNT = new NamespacedKey(CashClashPlugin.getInstance(), "supply_amount");
 
     private Keys() {
         throw new AssertionError("Nope.");
