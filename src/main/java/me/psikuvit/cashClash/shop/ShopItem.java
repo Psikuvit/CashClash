6863ff38842d7cf141845d2585f3ee0e4a2112ec
Psikuvit <thegengestar@gmail.com>
2025-12-13 23:15:04 +0100

    Fix: Added missing custom armors (ShopItem)

--- src/main/java/me/psikuvit/cashClash/shop/ShopItem.java
+++ src/main/java/me/psikuvit/cashClash/shop/ShopItem.java
@@ -31,19 +31,28 @@ public enum ShopItem {
     NETHERITE_CHESTPLATE(Material.NETHERITE_CHESTPLATE, ShopCategory.ARMOR, 25000),
 
     // Custom armor (Cash Clash exclusives)
-    INVESTORS_BOOTS(Material.IRON_BOOTS, ShopCategory.ARMOR, 4250),
-    INVESTORS_HELMET(Material.IRON_HELMET, ShopCategory.ARMOR, 4500),
-    INVESTORS_LEGGINGS(Material.IRON_LEGGINGS, ShopCategory.ARMOR, 4750),
-    INVESTORS_CHESTPLATE(Material.IRON_CHESTPLATE, ShopCategory.ARMOR, 5000),
+    INVESTORS_BOOTS(Material.IRON_BOOTS, ShopCategory.CUSTOM_ARMOR, 4250),
+    INVESTORS_HELMET(Material.IRON_HELMET, ShopCategory.CUSTOM_ARMOR, 4500),
+    INVESTORS_LEGGINGS(Material.IRON_LEGGINGS, ShopCategory.CUSTOM_ARMOR, 4750),
+    INVESTORS_CHESTPLATE(Material.IRON_CHESTPLATE, ShopCategory.CUSTOM_ARMOR, 5000),
 
-    TAX_EVASION_PANTS(Material.LEATHER_CHESTPLATE, ShopCategory.ARMOR, 5000),
-    GILLIE_SUIT_HAT(Material.IRON_HELMET, ShopCategory.ARMOR, 7500),
-    LIGHTFOOT_SHOES(Material.LEATHER_BOOTS, ShopCategory.ARMOR, 15000),
-    GUARDIANS_VEST(Material.DIAMOND_CHESTPLATE, ShopCategory.ARMOR, 20000),
-    FLAMEBRINGER_BOOTS(Material.DIAMOND_BOOTS, ShopCategory.ARMOR, 15000),
-    FLAMEBRINGER_LEGGINGS(Material.DIAMOND_LEGGINGS, ShopCategory.ARMOR, 20000),
-    DEATHMAULER_OUTFIT(Material.NETHERITE_CHESTPLATE, ShopCategory.ARMOR, 50000),
-    DRAGON_SET(Material.DIAMOND_CHESTPLATE, ShopCategory.ARMOR, 75000),
+    TAX_EVASION_PANTS(Material.LEATHER_LEGGINGS, ShopCategory.CUSTOM_ARMOR, 5000),
+
+    MAGIC_HELMET(Material.IRON_HELMET, ShopCategory.CUSTOM_ARMOR, 7500),
+
+    BUNNY_SHOES(Material.LEATHER_BOOTS, ShopCategory.CUSTOM_ARMOR, 15000),
+
+    GUARDIANS_VEST(Material.DIAMOND_CHESTPLATE, ShopCategory.CUSTOM_ARMOR, 20000),
+
+    FLAMEBRINGER_BOOTS(Material.DIAMOND_BOOTS, ShopCategory.CUSTOM_ARMOR, 15000),
+    FLAMEBRINGER_LEGGINGS(Material.DIAMOND_LEGGINGS, ShopCategory.CUSTOM_ARMOR, 20000),
+
+    DEATHMAULER_CHESTPLATE(Material.NETHERITE_CHESTPLATE, ShopCategory.CUSTOM_ARMOR, 25000),
+    DEATHMAULER_LEGGINGS(Material.NETHERITE_LEGGINGS, ShopCategory.CUSTOM_ARMOR, 25000),
+
+    DRAGON_CHESTPLATE(Material.DIAMOND_CHESTPLATE, ShopCategory.CUSTOM_ARMOR, 25000),
+    DRAGON_BOOTS(Material.DIAMOND_BOOTS, ShopCategory.CUSTOM_ARMOR, 25000),
+    DRAGON_HELMET(Material.DIAMOND_HELMET, ShopCategory.CUSTOM_ARMOR, 25000),
 
     // Food
     BREAD(Material.BREAD, ShopCategory.FOOD, 10, 4),
@@ -63,7 +72,7 @@ public enum ShopItem {
     // Utility
     LAVA_BUCKET(Material.LAVA_BUCKET, ShopCategory.UTILITY, 1500),
     COBWEB(Material.COBWEB, ShopCategory.UTILITY, 625, 4),
-    CROSSBOW(Material.CROSSBOW, ShopCategory.UTILITY, 4500, 4),
+    CROSSBOW(Material.CROSSBOW, ShopCategory.UTILITY, 4500),
     BOW(Material.BOW, ShopCategory.UTILITY, 4000),
     FISHING_ROD(Material.FISHING_ROD, ShopCategory.UTILITY, 1500),
     ARROWS(Material.ARROW, ShopCategory.UTILITY, 50, 5),
@@ -112,12 +121,12 @@ public enum ShopItem {
         return switch (this) {
             case INVESTORS_BOOTS, INVESTORS_HELMET, INVESTORS_LEGGINGS, INVESTORS_CHESTPLATE -> "<gray>Part of the Investor's set. Each piece increases money bonuses.</gray>";
             case TAX_EVASION_PANTS -> "<gray>Reduces death penalty and grants a small timeout bonus for staying alive.</gray>";
-            case GILLIE_SUIT_HAT -> "<gray>Stand still to become invisible for a short time. Cooldown applies.</gray>";
-            case LIGHTFOOT_SHOES -> "<gray>Activate to gain Speed II + Jump Boost I for 15s. 25s cooldown.</gray>";
+            case MAGIC_HELMET -> "<gray>Stand still to become invisible for a short time. Cooldown applies.</gray>";
+            case BUNNY_SHOES -> "<gray>Activate to gain Speed II + Jump Boost I for 15s. 25s cooldown.</gray>";
             case GUARDIANS_VEST -> "<gray>Provides Resistance II when low on health (limited uses per round).</gray>";
             case FLAMEBRINGER_BOOTS, FLAMEBRINGER_LEGGINGS -> "<gray>Part of the Flamebringer set: grants blue-fire effects on hit and permanent fire-resistance for boots.</gray>";
-            case DEATHMAULER_OUTFIT -> "<gray>Chest + Leggings: Kills heal you and can grant absorption.</gray>";
-            case DRAGON_SET -> "<gray>Full Dragon set: regen on hit, speed boosts and double-jump ability. Immune to knife damage/explosives.</gray>";
+            case DEATHMAULER_CHESTPLATE, DEATHMAULER_LEGGINGS -> "<gray>Chest + Leggings: Kills heal you and can grant absorption.</gray>";
+            case DRAGON_BOOTS, DRAGON_CHESTPLATE, DRAGON_HELMET -> "<gray>Full Dragon set: regen on hit, speed boosts and double-jump ability. Immune to knife damage/explosives.</gray>";
             default -> "";
         };
     }
