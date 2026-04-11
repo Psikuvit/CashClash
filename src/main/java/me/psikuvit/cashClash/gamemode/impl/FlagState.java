6a576cce7887e3a0b452809f877383b66b344123
Psikuvit <thegengestar@gmail.com>
2026-04-11 20:36:18 +0100

    Refactor: Remove flag base from FlagState and calculate it on-demand

--- src/main/java/me/psikuvit/cashClash/gamemode/impl/FlagState.java
+++ src/main/java/me/psikuvit/cashClash/gamemode/impl/FlagState.java
@@ -7,12 +7,12 @@ import java.util.UUID;
 
 /**
  * Record representing a flag state in Capture the Flag gamemode
- * Consolidates flag-related data: holder, capture time, base location, plate location, and banner
+ * Consolidates flag-related data: holder, capture time, capture plate (banner location), and banner
+ * Note: Flag base is always capture plate + 2 blocks up, so it's calculated on-demand
  */
 public record FlagState(
         UUID holder,
         long captureTime,
-        Location flagBase,
         Location capturePlate,
         BlockDisplay bannerDisplay,
         double bannerAngle
@@ -21,49 +21,49 @@ public record FlagState(
      * Create a new flag state with default values
      */
     public static FlagState create() {
-        return new FlagState(null, 0, null, null, null, 0.0);
+        return new FlagState(null, 0, null, null, 0.0);
     }
 
     /**
      * Create a flag state with a holder picked up
      */
     public FlagState withHolder(UUID holderUuid, long captureTime) {
-        return new FlagState(holderUuid, captureTime, this.flagBase, this.capturePlate, this.bannerDisplay, this.bannerAngle);
+        return new FlagState(holderUuid, captureTime, this.capturePlate, this.bannerDisplay, this.bannerAngle);
     }
 
     /**
      * Create a flag state with holder removed
      */
     public FlagState withoutHolder() {
-        return new FlagState(null, 0, this.flagBase, this.capturePlate, this.bannerDisplay, this.bannerAngle);
+        return new FlagState(null, 0, this.capturePlate, this.bannerDisplay, this.bannerAngle);
     }
 
     /**
-     * Create a flag state with base location set
-     */
-    public FlagState withBase(Location base) {
-        return new FlagState(this.holder, this.captureTime, base, this.capturePlate, this.bannerDisplay, this.bannerAngle);
-    }
-
-    /**
-     * Create a flag state with capture plate location set
+     * Create a flag state with capture plate location set (banner rotates around this)
      */
     public FlagState withCapturePlate(Location plate) {
-        return new FlagState(this.holder, this.captureTime, this.flagBase, plate, this.bannerDisplay, this.bannerAngle);
+        return new FlagState(this.holder, this.captureTime, plate, this.bannerDisplay, this.bannerAngle);
     }
 
     /**
      * Create a flag state with banner display set
      */
     public FlagState withBannerDisplay(BlockDisplay banner) {
-        return new FlagState(this.holder, this.captureTime, this.flagBase, this.capturePlate, banner, this.bannerAngle);
+        return new FlagState(this.holder, this.captureTime, this.capturePlate, banner, this.bannerAngle);
     }
 
     /**
      * Create a flag state with updated banner angle
      */
     public FlagState withBannerAngle(double angle) {
-        return new FlagState(this.holder, this.captureTime, this.flagBase, this.capturePlate, this.bannerDisplay, angle);
+        return new FlagState(this.holder, this.captureTime, this.capturePlate, this.bannerDisplay, angle);
+    }
+
+    /**
+     * Get flag base location (2 blocks above capture plate)
+     */
+    public Location getFlagBase() {
+        return capturePlate != null ? capturePlate.clone().add(0, 2, 0) : null;
     }
 
     /**
